b3d6b34b9f8d4dd61d382b30aef34148f5464b04
monicah pirisi <monicah.lekupe@ashesi.edu.gh>
2025-11-08 23:06:54 +0000

    Initial commit

---
+++ settings/db_cred.php
@@ -0,0 +1,27 @@
+<?php
+//Database credentials
+// Settings/db_cred.php
+
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'dbforlab');
+
+
+if (!defined("SERVER")) {
+    define("SERVER", "localhost");
+}
+
+if (!defined("USERNAME")) {
+    define("USERNAME", "monicah.lekupe");
+}
+
+if (!defined("PASSWD")) {
+    define("PASSWD", "Amelia@2026");
+}
+
+if (!defined("DATABASE")) {
+    // Use the database name from the provided SQL dump
+    define("DATABASE", "ecommerce_2025A_monicah_lekupe");
+}
+?>
